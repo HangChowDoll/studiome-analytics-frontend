21422204a744399f1f015446f3a3b80d9fd70d44
HangChowDoll <117353419+HangChowDoll@users.noreply.github.com>
2021-07-13 19:27:42 +0000

    Javadoc optimized

--- src/main.cpp
+++ src/main.cpp
@@ -64,4 +64,4 @@ TickTwo sleep_timeout(deep_sleep, SLEEP_TIMEOUT * 60000, 0);
 TickTwo button_sleep_timeout(deep_sleep, SLEEP_BUTTON_TIMEOUT * 1000, 1);
 
 void stop_all_timers () {
-  poll_in
+  poll_interval
