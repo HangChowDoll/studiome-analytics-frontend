121f5fc3feabe3325b1c0de0a803ecd0ee0389d0
HangChowDoll <117353419+HangChowDoll@users.noreply.github.com>
2021-10-10 22:08:05 +0000

    Remove period

--- src/main.cpp
+++ src/main.cpp
@@ -104,4 +104,4 @@ void read_dpad () {
 void read_buttons () {
   joy.set_button_state(BluJoy::BUTTON_A, digitalRead(BUTTON_A_PIN));
   joy.set_button_state(BluJoy::BUTTON_B, digitalRead(BUTTON_B_PIN));
-  joy.
+  joy.set_button_st
