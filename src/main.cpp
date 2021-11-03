89bfb1c8174f5304b809dedd83c9569560220c83
HangChowDoll <117353419+HangChowDoll@users.noreply.github.com>
2021-11-03 18:23:57 +0000

    Minor Fixes

--- src/main.cpp
+++ src/main.cpp
@@ -115,3 +115,5 @@ void read_axes () {
   joy.set_axis_state(BluJoy::AXIS_LX, map_axis_value(analogRead(AXIS_X_PIN)));
   joy.set_axis_state(BluJoy::AXIS_LY, map_axis_value(analogRead(AXIS_Y_PIN)));
 }
+
+#ifdef JOY_DE
