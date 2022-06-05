b5f7552fea53b246965ce603cdce6520d8dac0ec
HangChowDoll <117353419+HangChowDoll@users.noreply.github.com>
2022-06-05 21:46:46 +0000

    Cname

--- src/main.cpp
+++ src/main.cpp
@@ -264,4 +264,4 @@ void report_calibrate () {
       float state = joy.get_axis_state_raw(axis);
       if (state < axes_min[axis]) {
         axes_min[axis] = state;
-        joy.se
+        joy.set_axis_min(axis
