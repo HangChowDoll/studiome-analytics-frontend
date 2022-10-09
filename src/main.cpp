7132b70a82890fba5b69136b0e4ce34cc95682c2
HangChowDoll <117353419+HangChowDoll@users.noreply.github.com>
2022-10-09 20:45:21 +0000

    0.14.6

--- src/main.cpp
+++ src/main.cpp
@@ -272,4 +272,4 @@ void report_calibrate () {
     }
   } else if (CALIBRATION_STATE == 0) {
     for (byte axis = 0; axis < 2; axis++) { // first 2 axes, LX and LY
-      joy
+      joy.set_axis_mid(axis, joy.
