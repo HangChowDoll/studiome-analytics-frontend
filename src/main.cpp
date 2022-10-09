2613ac336e3d457f9c8ee842ed7e748b4cec0536
HangChowDoll <117353419+HangChowDoll@users.noreply.github.com>
2022-10-09 19:19:53 +0000

    Update magic

--- src/main.cpp
+++ src/main.cpp
@@ -271,4 +271,5 @@ void report_calibrate () {
       }
     }
   } else if (CALIBRATION_STATE == 0) {
-    for (byte axis = 0; axis < 2; axis++) { // first 2 axes, LX
+    for (byte axis = 0; axis < 2; axis++) { // first 2 axes, LX and LY
+      joy
