eb0fc1c60a88e7c00f003b047d501a7a7431a554
HangChowDoll <117353419+HangChowDoll@users.noreply.github.com>
2023-02-05 18:18:49 +0000

    Fixed Changelog

--- src/main.cpp
+++ src/main.cpp
@@ -379,4 +379,4 @@ void debug_common () {
   }
   Serial.print(" :: ");
   for (byte axis = 0; axis < BluJoy::AXIS_COUNT; axis++) {
-    const std::string spacer = (a
+    const std::string spacer = (axis < BluJoy::AXIS
