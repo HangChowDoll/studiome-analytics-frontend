0b3a31579ec02d7535ab5a4419f513f0e113fc4a
HangChowDoll <117353419+HangChowDoll@users.noreply.github.com>
2023-02-26 09:57:24 +0000

    update about page

--- src/main.cpp
+++ src/main.cpp
@@ -386,4 +386,4 @@ void debug_common () {
     );
   }
   Serial.print(":: DP: " + String(joy.get_dpad_state()) + "\n");
-  Serial.print("report runs: " + String(report_runs
+  Serial.print("report runs: " + String(report_runs) + ", poll runs: "
